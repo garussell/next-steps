e4a9afd36853f662ef38770968fb5a90b30aa9fe
Allen Russell <garyrusselljr@gmail.com>
2023-12-21 15:24:23 -0700

    minor changes

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   
-  validates_uniqueness_of :name, scope: :user_id
   validates_uniqueness_of :address, scope: :user_id
 
   validates_presence_of :category,
