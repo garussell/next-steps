417a9e7850c9d4c9e64bb2b074f37676d92c4011
Allen Russell <garyrusselljr@gmail.com>
2024-11-25 17:23:11 -0700

    fixing redirect url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.credentials.google[:GOOGLE_CLIENT_ID], Rails.application.credentials.google['GOOGLE_CLIENT_SECRET']
   {
     scope: 'email',
-    redirect_uri: Rails.application.credentials.google['GOOGLE_REDIRECT_URI']
+    # redirect_uri: Rails.application.credentials.google['GOOGLE_REDIRECT_URI']
   }
 end
 OmniAuth.config.allowed_request_methods = [:post, :get]
